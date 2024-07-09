327dddd4b95afe08e3cd97fbfc56b85df467f10c
Daniele <daniele@mail.com>
2024-07-09 18:31:05 +0200

    aggiunto sistema punteggi (punto 2)

--- script.js
+++ script.js
@@ -5,11 +5,15 @@ const optionsContainer = document.querySelector('.options-container');
 const nextBtn = document.querySelector('.next-btn');
 const resultText = document.getElementById('result');
 const changeThemeButton = document.querySelector('.changetheme-btn');
+const scoreText = document.querySelector('.score');
 let responseButtons = [];
 let countries = [];
 let currentCountry = {};
 let index = 0;
 let buttonIndex = 0;
+let score = 0;
+
+scoreText.style.visibility = 'hidden';
 
 async function fetchCountries() {
     const response = await fetch('https://restcountries.com/v3.1/all');
@@ -50,11 +54,15 @@ function checkAnswer(selectedCountry) {
         resultText.style.visibility = 'visible';
         resultText.textContent = 'Corretto!';
         resultText.style.color = 'green';
+        score++;
     } else {
         resultText.style.visibility = 'visible';
         resultText.innerText = `Sbagliato! La risposta corretta era:\n${currentCountry.name.common}.`;
         resultText.style.color = 'red';
+        score--;
     }
+    scoreText.style.visibility = 'visible';
+    scoreText.innerText = `Il tuo punteggio è...\n${score}!`;
 
 }
 
@@ -73,7 +81,7 @@ function changeTheme() {
         index++;
     }
     quizContainer.classList.toggle("new-quiz-container");
-   
+
     nextBtn.classList.toggle("new-next-btn");
     changeThemeButton.classList.toggle("new-changetheme-btn");
 }
